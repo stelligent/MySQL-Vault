6b85a99bb6a25a33bb9a15c4b494ad75114f8b7d
Dnyanesh Ghadge <nyy021@gmail.com>
2017-04-05 11:15:14 -0400

    Added token authentication and policies

--- vault/source_env.sh
+++ vault/source_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VAULT_TOKEN=4d1fb1a1-42b7-d910-5949-08ea6d305993
+#export VAULT_TOKEN=4d1fb1a1-42b7-d910-5949-08ea6d305993
 export VAULT_ADDR=https://localhost:8200
 export VAULT_SKIP_VERIFY=false
 export VAULT_CAPATH=`pwd`/certificates/ca/certs/ca.cert.pem
